7b0650702c9ab7d7a62bf84ab70ba5a6fdb8af57
CAndRyan <CAndRyan@users.noreply.github.com>
2022-07-09 07:18:32 -0400

    Add basic sentence extraction

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A tool for ingesting & parsing judicial news.
 
 e.g. Extract data from Knox County, TN District Attorney General's office's [press releases](https://dag.knoxcountytn.gov/media-public-info/press-releases/).
+
+## Technology Considerations
+
+* [NLP.js](https://github.com/axa-group/nlp.js)
+* [Compromise](https://github.com/spencermountain/compromise/)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('TESTING STUFF');
+import nlp from 'compromise';
+
+const sourceUrl = "https://dag.knoxcountytn.gov/press-release/stepfather-convicted-of-molesting-eleven-year-old/";
+const text = 'Prosecutors in DA Charme Allen’s Child Abuse Unit obtained convictions against a stepfather who molested his stepdaughter and sent her text messages soliciting sexual activity.  Cristobal Jose Vasquez, 39, was convicted of Solicitation of a Minor and Aggravated Sexual Battery.  Judge Steve Sword revoked Vasquez’s bond, ordered him into custody, and set the case for sentencing on August 11.';
+const doc = nlp(text);
+const sentences = doc.sentences().out('array');
+const data = {
+    sourceUrl,
+    sentences
+};
+
+console.log(data);
